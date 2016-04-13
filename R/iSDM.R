fa2a04389ee352de0f2de0197021f299fbe3c641
Hattab Tarek <hattab.tarek@gmail.com>
2016-04-13 16:04:49 +0200

    Added files via upload

---
+++ R/iSDM.R
@@ -0,0 +1,4 @@
+#' \code{\link{roxygenize}}.
+#'@description iSDM is an open-source R package that implements a few functions useful for modeling the spatial distribution of invasive species
+#'@details This package contains the following 4 functions: EnvSysSample, invasionForce, likelihoodCA, RGBplot
+#'@references Tarek Hattab, Ruben Van De Kerchove, Ben Somers, Boris Brasseur, Carol Ximena Garz?n L?pez, Duccio Rocchini, Emilie Gallet-Moron, Fabien Spicher, Guillaume Decocq, Hannes Feilhauer, Hélène Horen, Jens Warrie, Michael Ewald, Olivier Honnay, Pieter Kempeneers, Raf Aerts, Sandra Skowronek, Sebastian Schmidtlein and Jonathan Lenoir (In prep).An unified framework to model the potential and realized distributions of invasive species within the invaded range
